b98fd46968374810711b848e78ec46460c76dbf1
Max Shymchuk <octorix@mail.ru>
2020-10-12 05:35:55 +0300

    feat: update project base

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './styles/app.scss';
+import {Controls} from "./components/Controls/Controls";
+import {Notes} from "./components/Notes/Notes";
+import {Fab} from "@material-ui/core";
+import ArrowUpwardOutlinedIcon from '@material-ui/icons/ArrowUpwardOutlined';
 
 function App() {
+
+  const handleClick = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
   return (
-      <></>
+      <main className="container">
+        <Controls />
+        <Notes />
+        <Fab className="upper" color="primary" onClick={handleClick}>
+          <ArrowUpwardOutlinedIcon />
+        </Fab>
+      </main>
   );
 }
 
