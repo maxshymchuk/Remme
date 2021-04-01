8a311b9cb98c33172b0bc0ba09c9615baa38b38c
Max Shymchuk <octorix@mail.ru>
2021-04-02 02:00:43 +0300

    Updates

---
+++ precache-manifest.190388c5269104262b6a2170de43b9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f469708d53d599ec1b17705889121b7",
+    "url": "/Remme/index.html"
+  },
+  {
+    "revision": "1f8be48c6a3d441bbc60",
+    "url": "/Remme/static/css/main.0ac73293.chunk.css"
+  },
+  {
+    "revision": "3bf8a4e40124d9bfe5e3",
+    "url": "/Remme/static/js/2.4f80a5e5.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/Remme/static/js/2.4f80a5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8be48c6a3d441bbc60",
+    "url": "/Remme/static/js/main.c75ec1f5.chunk.js"
+  },
+  {
+    "revision": "2393e5a63e5de028bd77",
+    "url": "/Remme/static/js/runtime-main.06f13cf5.js"
+  }
+]);
